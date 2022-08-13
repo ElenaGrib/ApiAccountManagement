d61cb2e04f31366a1c0333d8975996fea939cf0a
ElenaGrib <elenagrib27@gmail.com>
2022-08-13 21:12:02 +0300

    Initial commit

---
+++ src/main/java/com/grib/api/accountmanagement/photoappapiaccountmanagement/PhotoAppApiAccountManagementApplication.java
@@ -0,0 +1,15 @@
+package com.grib.api.accountmanagement.photoappapiaccountmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class PhotoAppApiAccountManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotoAppApiAccountManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/grib/api/accountmanagement/photoappapiaccountmanagement/PhotoAppApiAccountManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grib.api.accountmanagement.photoappapiaccountmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppApiAccountManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
